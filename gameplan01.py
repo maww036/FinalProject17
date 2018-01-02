a77a8c04fe8a19833aa1d5b02ce06b84f8cb211f
mwilliams <mwest-williams@ucvts.tec.nj.us>
2018-01-02 21:06:50 +0000

    Updated Info

---
+++ gameplan01.py
@@ -0,0 +1,7 @@
+def introduce():
+    name = input("What is your name? ").lower()
+    age = input("How old are you? ")
+    gender = input("Are you a boy or a girl? ").lower()
+    print ("Welcome", name, "to the start of the game! You are a" ,age, "year-old", gender, "with a passion for surfing!")
+
+introduce()
